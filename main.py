14fb9ac2fc169448e42c6d6e210b19df2e37e259
bgmtasci07 <tascibegumsila@gmail.com>
2025-05-13 10:10:34 +0300

    main.py dosyası yüklendi.

---
+++ main.py
@@ -0,0 +1,10 @@
+import arcade
+from oyun import YarisOyunu
+
+def main():
+    """Ana fonksiyon - oyunu başlatır"""
+    window = YarisOyunu()
+    arcade.run()
+
+if __name__ == "__main__":
+    main()
